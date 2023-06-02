5d18e0f40deb04b589c9dd14c1e8b5928382d360
Neeraj Verma <113288731+NeerajVerma2001@users.noreply.github.com>
2023-06-02 19:57:04 +0530

    added Studentt.java

---
+++ Studentt.java
@@ -0,0 +1,15 @@
+package oops;
+
+public class Studentt 
+{
+	String sname;
+	int roll_no;
+	
+	public Studentt(String sname, int roll_no) 
+	{
+		this.sname=sname;
+		this.roll_no=roll_no;
+	}
+	
+	
+}
