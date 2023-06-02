126ce62d577d28434eea8ff63beab4c2ae9e809a
Neeraj Verma <113288731+NeerajVerma2001@users.noreply.github.com>
2023-06-02 19:58:05 +0530

    added StuDriver.java

---
+++ StuDriver.java
@@ -0,0 +1,10 @@
+package oops;
+
+public class StuDriver 
+{
+	public static void main(String[] args) 
+	{
+		Classroomm c=new Classroomm(4, new Student("neeraj",5)); 
+				c.classroommDetails();
+	}
+}
